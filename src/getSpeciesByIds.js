cafd2bc61eab115862e2f3be08f37e412d65e460
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-10-31 18:04:16 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const idAnimals = species.filter((idAnimal) => ids.includes(idAnimal.id))
+  if (ids === undefined) {
+    return [];
+  }
+  return (idAnimals)
 }
 
 module.exports = getSpeciesByIds;
+
