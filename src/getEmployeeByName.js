0e604fea978ccca1750e74668fc7be97d26cf354
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-01 15:23:57 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const names = employees.some((nomeColaborador) => nomeColaborador.firstName === employeeName);
+  if (employeeName === undefined) {
+    return {};
+  } if (names === true) {
+    return employees.find((nomeColaborador) => nomeColaborador.firstName === employeeName);
+  }
+  return employees.find((sobrenomeColaborador) => sobrenomeColaborador.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
