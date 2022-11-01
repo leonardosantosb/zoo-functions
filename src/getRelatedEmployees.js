6928fdb9c37014a9e766be80290d5c8827453701
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-01 18:20:29 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = employees.some((manager) => manager.managers
+    .some((gerente1) => gerente1 === id));
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const nomes = employees.filter((nome) => nome.managers.includes(managerId));
+    const nomesFiltrados = nomes
+      .map((nomeFiltrado) => `${nomeFiltrado.firstName} ${nomeFiltrado.lastName}`);
+    return nomesFiltrados;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
