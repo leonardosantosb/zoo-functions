034dc409e19f0c8ba63490d7adefcef213c7db82
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-10-31 22:07:34 -0300

    refatorando

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,12 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
+function getSpeciesByIds(...ids){
   const idAnimals = species.filter((idAnimal) => ids.includes(idAnimal.id))
   if (ids === undefined) {
-    return [];
-  }
+    return []
+  };
   return (idAnimals)
-}
+};
 
 module.exports = getSpeciesByIds;
-
