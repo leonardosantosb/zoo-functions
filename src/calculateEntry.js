79613a62b799fa0fb93d6da624bb92ea0252cc44
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-04 15:53:24 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const child = (entrants.filter((entrant) => entrant.age < 18)).length;
+  const adult = (entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50)).length;
+  const senior = (entrants.filter((entrant) => entrant.age >= 50)).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { ...price } = data.prices;
+  const preco = countEntrants(entrants);
+  return ((preco.child * price.child) + (preco.adult * price.adult)
+   + (preco.senior * price.senior));
 }
 
 module.exports = { calculateEntry, countEntrants };
