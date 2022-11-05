1c5f2cead05009de10c7ff61c66b5107c86efb2f
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-05 16:13:56 -0300

    refatorando

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,10 +3,9 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const pessoaResponsavel = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
-
-  const animais = species.find((animal) => animal.id === pessoaResponsavel)
-  const maisVelho = animais.residents.sort((a, b) => b.age - a.age) [0];  
-    
-  return Object.values(maisVelho)
+  const animais = species.find((animal) => animal.id === pessoaResponsavel);
+  const maisVelho = animais.residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(maisVelho);
 }
+
 module.exports = getOldestFromFirstSpecies;
