5708579be8623a0bfb196849eceb07100d372b4e
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-05 15:57:23 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  const pessoaResponsavel = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
 
+  const animais = species.find((animal) => animal.id === pessoaResponsavel)
+  const maisVelho = animais.residents.sort((a, b) => b.age - a.age) [0];  
+    
+  return Object.values(maisVelho)
+}
 module.exports = getOldestFromFirstSpecies;
