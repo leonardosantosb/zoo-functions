080dc1897cb7ed698a1b95be734ea13656914913
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-07 00:17:14 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, cur) => {
+      acc[cur.name] = cur.residents.length;
+      return acc;
+    }, {});
+  }
+  const acharSpecie = species.find((specie) => specie.name === animal.specie);
+  const acharSexo = acharSpecie.residents.filter((acharSex) => acharSex.sex === animal.sex);
+  if (!animal.sex) {
+    return acharSpecie.residents.length;
+  }
+  return acharSexo.length;
 }
 
 module.exports = countAnimals;
