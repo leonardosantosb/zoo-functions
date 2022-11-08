1f8dd265f2c905525ec5f2747035c8e272434a37
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-08 18:16:41 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+  const vazio = [];
+  const listaVazia = data.employees.forEach((employee) =>
+    vazio.push({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: data.species.filter((specie) =>
+        employee.responsibleFor.includes(specie.id))
+        .map((nomeEspecie) => nomeEspecie.name),
+      locations: data.species.filter((specie) =>
+        employee.responsibleFor.includes(specie.id))
+        .map((lugarEspecie) => lugarEspecie.location),
+    }));
+  
+  function getEmployeesCoverage(obj) {
+    if (obj === undefined) return vazio;
+    const { name, id } = obj;
+    if (vazio.find((employee) => employee.id === id)) {
+      return vazio.find((employee) => employee.id === id);
+    }
+    if (vazio.find((employee) => employee.fullName.includes(name))) {
+      return vazio.find((employee) => employee.fullName.includes(name));
+    }
+    throw new Error('Informações inválidas');
+  }
+
 
 module.exports = getEmployeesCoverage;
