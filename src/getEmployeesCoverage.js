c555b06232767f099b5c1529069f83f83bddd997
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-08 18:20:54 -0300

    refatorando

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,29 +1,28 @@
 const data = require('../data/zoo_data');
 
-  const vazio = [];
-  const listaVazia = data.employees.forEach((employee) =>
-    vazio.push({
-      id: employee.id,
-      fullName: `${employee.firstName} ${employee.lastName}`,
-      species: data.species.filter((specie) =>
-        employee.responsibleFor.includes(specie.id))
-        .map((nomeEspecie) => nomeEspecie.name),
-      locations: data.species.filter((specie) =>
-        employee.responsibleFor.includes(specie.id))
-        .map((lugarEspecie) => lugarEspecie.location),
-    }));
-  
-  function getEmployeesCoverage(obj) {
-    if (obj === undefined) return vazio;
-    const { name, id } = obj;
-    if (vazio.find((employee) => employee.id === id)) {
-      return vazio.find((employee) => employee.id === id);
-    }
-    if (vazio.find((employee) => employee.fullName.includes(name))) {
-      return vazio.find((employee) => employee.fullName.includes(name));
-    }
-    throw new Error('Informações inválidas');
-  }
+const vazio = [];
+data.employees.forEach((employee) =>
+  vazio.push({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: data.species.filter((specie) =>
+      employee.responsibleFor.includes(specie.id))
+      .map((nomeEspecie) => nomeEspecie.name),
+    locations: data.species.filter((specie) =>
+      employee.responsibleFor.includes(specie.id))
+      .map((lugarEspecie) => lugarEspecie.location),
+  }));
 
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) return vazio;
+  const { name, id } = obj;
+  if (vazio.find((employee) => employee.id === id)) {
+    return vazio.find((employee) => employee.id === id);
+  }
+  if (vazio.find((employee) => employee.fullName.includes(name))) {
+    return vazio.find((employee) => employee.fullName.includes(name));
+  }
+  throw new Error('Informações inválidas');
+}
 
 module.exports = getEmployeesCoverage;
